8b5110d45749f3ac022f8af5f774b3a26b3fbfbe
Naomi Eniola <naomiadesiyan@hotmail.com>
2024-07-10 11:09:46 +0100

    Add project files.

---
+++ Fibonacci-2/Program.cs
@@ -0,0 +1,29 @@
+﻿//int f(int n) = int n-1 + int n-2;
+
+//for (int f = 0; f < 100; f++)
+//{
+
+//}
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        uint n = 20;
+        n--; // where the first number in sequence is 0
+        uint sequence = n;
+        // Console.WriteLine(sequence);
+        Console.WriteLine($"The answer for sequence {sequence} is {Fibonacci(n)}");
+    }
+
+    private static ulong Fibonacci(uint n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        return Fibonacci(n - 2) + Fibonacci(n - 1);
+    }
+}
